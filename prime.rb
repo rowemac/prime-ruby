1c57daddfd7e493966753d89cac3f71e5b688a66
Mac Rowe <m.rowe42@gmail.com>
2021-02-26 15:58:16 -0500

    commit tree 965189f95c3b9c83eb6a79a41af60926701eb56a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        false
+    else
+        (2..number-1).to_a.all? do | factor |
+            number % factor != 0
+        end
+    end
+
+end
+
